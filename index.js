3ecc2a2fac83436ef6c00d3f7db078cab90c9f3f
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-04-28 14:12:59 -0700

    feat: add to-do list item from user input

--- index.js
+++ index.js
@@ -13,9 +13,30 @@ function App() {
             isComplete: false
         }
     ])
+
+    const [value, setValue] = React.useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log(value)
+        if (!value) return
+        setTodos([...todos, {
+            text: value,
+            isCompleted: false
+        }])
+        setValue('')
+    }
+
     return (
         <>
             {todos.map((todo, idx) => <div key={idx}>{todo.text}</div>)}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Add to-do item..."
+                    value={value}
+                    onChange={e => setValue(e.target.value)} />
+            </form>
         </>
     )
 }
